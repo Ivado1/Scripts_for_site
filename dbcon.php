d6cfe177d04f69b39e1b6ac35d47a8953bb914d4
Ivan <136039284+Ivado1@users.noreply.github.com>
2025-04-19 10:29:43 +0800

    Add files via upload

---
+++ dbcon.php
@@ -0,0 +1,13 @@
+<?php
+$dbServername ="0.23.51.25";
+$dbUsername ="test";
+$dbPass= "pass";
+$dbName= "test";
+
+$conn=mysqli_connect($dbServername,$dbUsername,$dbPass,$dbName);
+
+//if($conn->connect_error){
+//die("Connect failed: " .$conn->connect_error);
+//}
+//echo "connection succefully";
+?>
